43396f4f1d53ab61d0a81a1d1a999a0a02e46945
Jordan Jefferson <jjeffe10@gmail.com>
2024-07-19 15:45:00 -0400

    Made an api namespace route for the UsersController. Gave the users resources a path of create-account.

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,16 @@
+module Api
+    class UsersController < ApplicationController
+        def create
+            @user = User.new(user_params)
+            if @user.save
+                render json: { success: true }, status: :created
+            else
+                render json: @user.errors, status: :unprocessable_entity
+            end
+        end
+        
+        def user_params
+            user_params = params.require(:user).permit(:username, :password)
+        end
+    end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get '/', to: 'application#render_react', as: :root
 
   get '/create-account', to: 'application#render_react'
-  post '/api/create-account', to: 'users#create'
+  # post '/api/create-account', to: 'users#create'
   get 'signup/*all', to: 'application#render_react', as: :signup
+
+  namespace :api do
+      resources :users, path: 'create-account', only: [:create]
+  end
 end
