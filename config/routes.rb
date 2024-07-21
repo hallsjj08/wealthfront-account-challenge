3ef025086981ac663f57e0366fc82c36e8897dd4
Jordan Jefferson <jjeffe10@gmail.com>
2024-07-20 23:09:22 -0400

    Added a route to catch all unmatched route requests to be sent to the client instead of the rails application erroring out. Let the client handle the error at the error boundary.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  match '*unmatched', to: 'application#render_react', via: :all
+
   resources :sessions, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
