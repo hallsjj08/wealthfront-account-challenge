6aa7c1ae8752bbea063bdbb5445edc0b7425b21b
Jordan Jefferson <jjeffe10@gmail.com>
2024-07-22 16:54:34 -0400

    Added tests for button component.

---
+++ app/frontend/reusable-components/button/button.test.tsx
@@ -0,0 +1,32 @@
+import { describe, expect, jest, test } from '@jest/globals';
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Button } from './button';
+import {Form } from 'react-router-dom';
+
+describe('CreateUser', () => {
+  test('test button of type button', () => {
+    const click = jest.fn();
+    render(<Button onClick={click}>Test Button</Button>)
+    const button = screen.getByText('Test Button');
+    expect(button.nodeName).toBe("BUTTON")
+    fireEvent.click(button);
+    expect(click).toBeCalled();
+  });
+
+  test('test button of type submit', () => {
+    const click = jest.fn();
+    render(
+        <Form onSubmit={click}>
+            <Button type='submit'>Submit Button</Button>
+        </Form>)
+    const button = screen.getByText('Submit Button');
+    console.log(button)
+    expect(button.nodeName).toBe("BUTTON")
+    fireEvent.click(button);
+    expect(click).toBeCalled();
+  })
+
+  test.todo('test button with href')
+});
